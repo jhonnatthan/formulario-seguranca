38714e89ed216ebe2de06f28f9fd40e57be2fb99
Jhonnatthan <jhonnatthans@outlook.com>
2019-11-26 10:51:22 -0300

    [ADD] - Lógica de Captcha back-end

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const User = use("App/Models/User");
+const Env = use("Env");
+const axios = use("axios");
 
 class LoginController {
     async create({ view, session }) {
@@ -9,12 +11,30 @@ class LoginController {
         return view.render("auth/login", { users: users.rows, error });
     }
     async store({ request, response, auth, session }) {
-        const data = request.only(["username", "password"]);
-        try {
-            await auth.attempt(data.username, data.password);
-            return response.route("dashboard.index");
-        } catch (error) {
-            session.put("error", "Login e/ou senha incorretos!");
+        const data = request.all();
+
+        if (data["g-recaptcha-response"] != null) {
+            console.log(Env.get("RECAPTCHA_SECRET"));
+            let url = "https://www.google.com/recaptcha/api/siteverify";
+            url = url + `?secret=${Env.get("RECAPTCHA_SECRET")}`;
+            url = url + `&response=${data["g-recaptcha-response"]}`;
+
+            const response = await axios.post(url);
+
+            if (response.data.success) {
+                try {
+                    await auth.attempt(data.username, data.password);
+                    return response.route("dashboard.index");
+                } catch (error) {
+                    session.put("error", "Login e/ou senha incorretos!");
+                    return response.route("login.create");
+                }
+            } else {
+                session.put("error", "Captcha inválido");
+                return response.route("login.create");
+            }
+        } else {
+            session.put("error", "Captcha inválido");
             return response.route("login.create");
         }
     }
